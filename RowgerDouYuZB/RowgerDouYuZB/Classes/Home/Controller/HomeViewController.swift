ad260a77fc21579db7df713bd47c2500954cc304
luojie <467021072@qq.com>
2018-08-08 09:44:53 +0800

    首页导航栏设置

--- RowgerDouYuZB/RowgerDouYuZB/Classes/Home/Controller/HomeViewController.swift
+++ RowgerDouYuZB/RowgerDouYuZB/Classes/Home/Controller/HomeViewController.swift
@@ -24,10 +24,17 @@ extension HomeViewController{
     }
     
     private func setupNavigationBar(){
+        //设置左侧
         let btn = UIButton()
         btn.setImage(UIImage(named: "logo"), for: .normal)
         btn.sizeToFit()
-        
         navigationItem.leftBarButtonItem=UIBarButtonItem(customView: btn)
+        
+        //设置右侧
+        let size = CGSize(width: 40, height: 40)
+        let historyItem=UIBarButtonItem(imageName: "image_my_history", hightImageName: "Image_my_history_click", size: size)
+        let searchItem=UIBarButtonItem(imageName: "btn_search", hightImageName: "btn_search_clicked", size: size)
+        let qrcodeItem=UIBarButtonItem(imageName: "Image_scan", hightImageName: "Image_scan_click", size: size)
+        navigationItem.rightBarButtonItems=[historyItem,searchItem,qrcodeItem]
     }
 }
